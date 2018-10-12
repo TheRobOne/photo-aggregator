c123263200c4f03cb6e6d801d6e73ceb649d3547
TheRobOne <m_rusek@vp.pl>
2018-10-12 22:37:49 +0200

    implemented search by keyword

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 import { SEARCH } from './types';
 
 //search for photos
-export const searchPhotos = () => dispatch => {
-    axios.get('https://pixabay.com/api/?key=6771879-3964c448f80d04a7a92b37074&q=car&image_type=photo&pretty=true&page=1&per_page=5')
+export const searchPhotos = (tag) => dispatch => {
+    axios.get(`https://pixabay.com/api/?key=6771879-3964c448f80d04a7a92b37074&q=${tag}&image_type=photo&pretty=true&page=1&per_page=5`)
         .then(res => {
             dispatch({
                 type: SEARCH,
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,18 +5,35 @@ import PropTypes from 'prop-types';
 
 class Navbar extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            searchInput: ''
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
     onSubmit(event) {
         event.preventDefault();
     
-        this.props.searchPhotos();
+        this.props.searchPhotos(this.state.searchInput);
       }
+
+    onChange(event) {
+        this.setState({ [event.target.name]: event.target.value})
+    }
+
 
     render() {
         return (
             <nav className="navbar navbar-light bg-light">
                 <a className="navbar-brand">Navbar</a>
                 <form className="form-inline" onSubmit={event => this.onSubmit(event)}>
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search for photos..." 
+                        aria-label="Search" name="searchInput" value={this.state.searchInput} onChange={this.onChange}
+                    />
                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </nav>
