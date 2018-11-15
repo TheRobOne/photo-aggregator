316fcfb5641401a0f7f0c398720aa7f69161eab6
TheRobOne <m_rusek@vp.pl>
2018-11-16 00:32:33 +0100

    removed unneeded code form Gallery component

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -5,7 +5,6 @@ import { withStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
 
@@ -43,7 +42,7 @@ class Gallery extends Component {
             open: false,
             currentPhoto: '',
             dialogType: '',
-            dialogMessage: 'You have to been logged in first.'
+            favUserPhotos: []
         }
         this.handleClose = this.handleClose.bind(this);
     }
@@ -53,51 +52,32 @@ class Gallery extends Component {
     }
 
     onClickStar(photo){
-        if (sessionStorage.getItem("userID")) {
-            const Photo = {
-                fullPhotoURL: photo.fullImageURL,
-                smallPhotoURL: photo.smallImageURL,
-                userID: sessionStorage.getItem("userID"),
-                author: photo.user,
-                authorURL: photo.userURL,
-                authorAvatar: photo.userAvatar,
-                tag: photo.tag,
-                provider: photo.provider
-            }
-            axios.post('http://localhost:4200/photos/favourite', Photo)
-                .then(res => {
-                })
-                .catch(err => {
-                    console.log("err");
-                    this.setState({open: true, currentPhoto: photo, dialogType: 'star', dialogMessage: 'Error while adding photo to favourities'});
-                })
-
-        } else {
-            this.setState({open: true, currentPhoto: photo});
+        const Photo = {
+            fullPhotoURL: photo.fullImageURL,
+            smallPhotoURL: photo.smallImageURL,
+            userID: sessionStorage.getItem("userID"),
+            author: photo.user,
+            authorURL: photo.userURL,
+            authorAvatar: photo.userAvatar,
+            tag: photo.tag,
+            provider: photo.provider
         }
+        axios.post('http://localhost:4200/photos/favourite', Photo)
+            .then(res => {
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     handleClose = () => {
-        this.setState({ open: false, dialogMessage: 'You have to been logged in first.' });
+        this.setState({ open: false});
       }
 
     render() {
         let { classes } = this.props;
 
         let photosList = null;
-        let dialogContent = null;
-
-        if(this.state.dialogType === 'fullscreen'){
-            dialogContent = (
-                <img src={this.state.currentPhoto.fullImageURL} alt="" style={{ width: '100%', height:'100%' }} />
-            );
-        } else if (!sessionStorage.getItem("userID") || this.state.dialogType === 'star') {
-            dialogContent = (
-                <DialogContent>
-                        <span>{this.state.dialogMessage}<br/><br/></span>
-                </DialogContent>
-            );
-        } 
 
         photosList = this.props.searchResults.searchResults.map((photo) => 
             <GridListTile key={photo.id} cols={1}>
@@ -115,7 +95,7 @@ class Gallery extends Component {
                     <Dialog
                         open={this.state.open}
                         onClose={this.handleClose}>
-                        {dialogContent}
+                        <img src={this.state.currentPhoto.fullImageURL} alt="" style={{ width: '100%', height:'100%' }} />
                     </Dialog>
                 </div>
                 <Pagination/>
